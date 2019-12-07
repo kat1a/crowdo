8a936a4a67c4ee37281eff19973e04967ab46263
Katia <katia_telit@hotmail.com>
2019-12-07 19:37:59 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'crowdo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'CrowDo';
+}
